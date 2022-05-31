4683db9c638875d0accb16a067bcd5763dcf0539
Stowy <stowy@protonmail.ch>
2022-05-31 22:11:40 +0200

    added the rooms with enemies

--- Assets/_Project/Enemies/MonsterSpawner.cs
+++ Assets/_Project/Enemies/MonsterSpawner.cs
@@ -16,12 +16,11 @@ namespace ProjectAwakening.Enemies
 		[SerializeField]
 		private DungeonEnemiesCountScriptableObject dungeonEnemiesCount;
 
-		[SerializeField]
-		private int spawnsLeft = 1;
+		[field: SerializeField] public int SpawnsLeft { get; private set; } = 1;
 
 		public void SpawnMonster()
 		{
-			if (spawnableMonsters.Count == 0 || spawnsLeft <= 0)
+			if (spawnableMonsters.Count == 0 || SpawnsLeft <= 0)
 				return;
 
 			// Store the transform in a var to not access it through the getter twice
@@ -36,7 +35,7 @@ namespace ProjectAwakening.Enemies
 				enemyLife.DungeonEnemiesCount = dungeonEnemiesCount;
 			}
 
-			spawnsLeft--;
+			SpawnsLeft--;
 		}
 
 		private GameObject GetRandomMonster() => spawnableMonsters[Random.Range(0, spawnableMonsters.Count)];
