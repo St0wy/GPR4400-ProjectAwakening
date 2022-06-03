53e82d9fe006365d02b5a1e83f74857a7e9c5690
dorF <nemoz.burkhalter@gmail.com>
2022-06-03 14:41:20 +0200

    Cadavers are now created on the gound

--- Assets/_Project/Enemies/EnemyLife.cs
+++ Assets/_Project/Enemies/EnemyLife.cs
@@ -19,8 +19,11 @@ namespace ProjectAwakening.Enemies
 			if (cadaverSprite != null)
 			{
 				var cadaver = new GameObject();
-				cadaver.AddComponent<SpriteRenderer>().sprite = cadaverSprite;
-				
+				SpriteRenderer cadaverSpriteRenderer = cadaver.AddComponent<SpriteRenderer>();
+				cadaverSpriteRenderer.sprite = cadaverSprite;
+				cadaverSpriteRenderer.sortingLayerID = SortingLayer.NameToID("Ground");
+				cadaverSpriteRenderer.sortingOrder = 1;
+
 				Transform tr = transform;
 				cadaver.transform.position = tr.position;
 				cadaver.transform.localScale = tr.localScale;
