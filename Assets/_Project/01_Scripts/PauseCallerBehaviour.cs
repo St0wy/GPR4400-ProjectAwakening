48af7a08f4d20e1e3cc776d3684bc6de00043797
Stowy <stowy@protonmail.ch>
2022-06-03 11:32:25 +0200

    added pause menu and changed product name

---
+++ Assets/_Project/01_Scripts/PauseCallerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using JetBrains.Annotations;
+using ProjectAwakening.UI;
+using UnityEngine;
+
+namespace ProjectAwakening
+{
+	public class PauseCallerBehaviour : MonoBehaviour
+	{
+		private PauseMenuBehaviour pause;
+
+		private void Awake()
+		{
+			pause = FindObjectOfType<PauseMenuBehaviour>();
+		}
+
+		[UsedImplicitly]
+		private void OnPause()
+		{
+			if (pause == null) return;
+
+			pause.TogglePause();
+		}
+	}
+}
---
+++ Assets/_Project/UI/PauseMenuBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using MyBox;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProjectAwakening.UI
+{
+	public class PauseMenuBehaviour : MonoBehaviour
+	{
+		[SerializeField] private GameObject content;
+		[SerializeField] private SceneReference mainMenuScene;
+		[SerializeField] private Button firstSelected;
+
+		private bool isPaused;
+
+		public bool IsPaused
+		{
+			get => isPaused;
+			private set
+			{
+				isPaused = value;
+				content.SetActive(isPaused);
+				Time.timeScale = isPaused ? 0f : 1f;
+			}
+		}
+
+		public void TogglePause()
+		{
+			if (IsPaused) Resume();
+			else Pause();
+		}
+
+		public void Pause()
+		{
+			IsPaused = true;
+			firstSelected.Select();
+		}
+
+		public void Resume()
+		{
+			IsPaused = false;
+		}
+
+		public void GoToMainMenu()
+		{
+			mainMenuScene.LoadScene();
+		}
+
+		public void QuitGame()
+		{
+			Application.Quit();
+		}
+	}
+}
